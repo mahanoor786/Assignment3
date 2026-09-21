cf7a82f96132decd20511ac4f18bd854a523382c
mahanoor786 <sayyadmahanoor62@gmail.com>
2026-09-21 10:30:09 +0530

    Updating homologs script

---
+++ find_homologs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+query=$1
+subject=$2
+output=$3
+
+tblastn -query "$query" -subject "$subject" \
+    -outfmt '6 qseqid sseqid pident length qlen' |
+awk '$3 > 30 && $4 > 0.90 * $5' > "$output"
+
+wc -l < "$output"
